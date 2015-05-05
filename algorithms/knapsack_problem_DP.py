a15266384f23279f216bf4cb32d925a8346772bf
Pavel <pavelsd@abv.bg>
2015-05-05 15:31:43 +0300

    Knapsack DP exceptions

--- algorithms/knapsack_problem_DP.py
+++ algorithms/knapsack_problem_DP.py
@@ -1,18 +1,24 @@
+from math import floor, ceil
+
+
 def knapsack_problem_DP(items, capacity):
     """Given a list of items (each item is a two-tuple (value, weight)),
-    where every item has a value and weight , 
+    where every item has a value and weight ,
     finds the highest value that can be
     achieved from a subset of the items subject to the constraint
     of the capacity.
     Since list and tuple behave in the same way with respect to
     indexing the function can work with tuples and lists for items,
     and each item can be a two-tuple or list of size 2.
+    Weights and capacity have to be positive integers.
+    If capacity is fraction it is rounded to the closest integer
+    below it. If item weight is fraction it is rounded to the closes
+    integer that is higher.
 """
     if capacity < 0:
         raise NegativeCapacityError
 
-    if not isinstance(items, (list, tuple)):
-        raise ItemsNotATupleOrListError
+    capacity = floor(capacity)
 
     number_of_items = len(items)
     knapsack_table = [[0 for x in range(capacity + 1)]
@@ -21,18 +27,8 @@ def knapsack_problem_DP(items, capacity):
     for index, item in enumerate(iter(items)):
         index += 1
 
-        try:
-            item_value = item[0]
-            item_weight = item[1]
-        except TypeError:
-            raise ItemNotATwoTuple
-
-        try:
-            item[2]
-        except:
-            pass
-        else:
-            raise ItemNotATwoTuple
+        item_value = item[0]
+        item_weight = ceil(item[1])
 
         if item_weight <= 0:
             raise ItemWithNonPositiveWeightError
@@ -54,13 +50,5 @@ class NegativeCapacityError(Exception):
     pass
 
 
-class ItemsNotATupleOrListError(Exception):
-    pass
-
-
 class ItemWithNonPositiveWeightError(Exception):
-    pass
-
-
-class ItemNotATwoTuple(Exception):
     pass
--- algorithms/knapsack_problem_DP_test.py
+++ algorithms/knapsack_problem_DP_test.py
@@ -16,6 +16,12 @@ class TestKnapsackDPoptimality(unittest.TestCase):
         answer = knapsack_problem_DP(items, capacity)
         self.assertEqual(answer, 40)
 
+    def test_knapsack_optimal_solution_exmp3(self):
+        items = [(1, 1), (6, 2), (18, 4.6), (22, 5.2), (28, 6.4)]
+        capacity = 11.5
+        answer = knapsack_problem_DP(items, capacity)
+        self.assertEqual(answer, 40)
+
 
 class TestKnapsackDPException(unittest.TestCase):
 
@@ -25,28 +31,10 @@ class TestKnapsackDPException(unittest.TestCase):
         with self.assertRaises(NegativeCapacityError):
             knapsack_problem_DP(items, capacity)
 
-    def test_items_not_a_list_or_tuple(self):
-        items = 3
-        capacity = 7
-        with self.assertRaises(ItemsNotATupleOrListError):
-            knapsack_problem_DP(items, capacity)
-
     def test_item_with_non_pisitive_weight(self):
         items = [(3, 4), (2, 3), (4, -2), (4, 3)]
         capacity = 6
         with self.assertRaises(ItemWithNonPositiveWeightError):
-            knapsack_problem_DP(items, capacity)
-
-    def test_item_not_a_two_tuple(self):
-        items = [(3, 4), (2, 3), 3, (4, 3)]
-        capacity = 6
-        with self.assertRaises(ItemNotATwoTuple):
-            knapsack_problem_DP(items, capacity)
-
-    def test_item_more_than_two_tuple(self):
-        items = ((3, 4), (2, 3, 5), (4, 2), (4, 3))
-        capacity = 6
-        with self.assertRaises(ItemNotATwoTuple):
             knapsack_problem_DP(items, capacity)
 
 
