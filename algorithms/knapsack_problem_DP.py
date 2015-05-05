5d3c6d47a4f8e96e2c6e4bb1d9b8574bc4f0dac3
Pavel <pavelsd@abv.bg>
2015-05-05 16:00:56 +0300

    Docstring update Knapsack DP

--- algorithms/knapsack_problem_DP.py
+++ algorithms/knapsack_problem_DP.py
@@ -2,14 +2,17 @@ from math import floor, ceil
 
 
 def knapsack_problem_DP(items, capacity):
-    """Given a list of items (each item is a two-tuple (value, weight)),
-    where every item has a value and weight ,
+    """Given a list of items (each item is a two-tuple with numbers
+    (value, weight)), where every item has a value and weight ,
     finds the highest value that can be
     achieved from a subset of the items subject to the constraint
-    of the capacity.
+    of the capacity (the sum of all included items must be
+    less than or equal to the capacity).
+
     Since list and tuple behave in the same way with respect to
     indexing the function can work with tuples and lists for items,
     and each item can be a two-tuple or list of size 2.
+
     Weights and capacity have to be positive integers.
     If capacity is fraction it is rounded to the closest integer
     below it. If item weight is fraction it is rounded to the closes
