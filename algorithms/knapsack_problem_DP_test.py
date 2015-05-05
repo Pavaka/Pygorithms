147e4cc82224eafce4f63f37a7baa283502f94e4
Pavel <pavelsd@abv.bg>
2015-05-05 15:55:20 +0300

    Knapsack DP exceptions changed and tests

--- algorithms/knapsack_problem_DP_test.py
+++ algorithms/knapsack_problem_DP_test.py
@@ -32,9 +32,33 @@ class TestKnapsackDPException(unittest.TestCase):
             knapsack_problem_DP(items, capacity)
 
     def test_item_with_non_pisitive_weight(self):
-        items = [(3, 4), (2, 3), (4, -2), (4, 3)]
+        items = [(3, 4), (2, 3), (4, -2), (4, -3)]
         capacity = 6
         with self.assertRaises(ItemWithNonPositiveWeightError):
+            knapsack_problem_DP(items, capacity)
+
+    def test_capacity_NaN(self):
+        items = [(3, 4), (2, 3), (4, 2), (4, 3)]
+        capacity = "Error"
+        with self.assertRaises(TypeError):
+            knapsack_problem_DP(items, capacity)
+
+    def test_weight_NaN(self):
+        items = [(3, "four"), (2, 3), (4, 2), (4, 3)]
+        capacity = 6
+        with self.assertRaises(TypeError):
+            knapsack_problem_DP(items, capacity)
+
+    def test_value_NaN(self):
+        items = [(3, 4), ("two", 3), (4, 2), (4, 3)]
+        capacity = 6
+        with self.assertRaises(TypeError):
+            knapsack_problem_DP(items, capacity)
+
+    def test_items_not_a_list(self):
+        items = 3
+        capacity = 6
+        with self.assertRaises(TypeError):
             knapsack_problem_DP(items, capacity)
 
 
