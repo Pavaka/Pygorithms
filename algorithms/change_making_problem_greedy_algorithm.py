cc4d141024228322ab4c10de3334df2ab9024d5e
Pavel <pavelsd@abv.bg>
2015-06-04 18:08:52 +0300

    Change making prolbem working. Docstring and tests added

--- algorithms/change_making_problem_greedy_algorithm.py
+++ algorithms/change_making_problem_greedy_algorithm.py
@@ -1,6 +1,15 @@
 def change_making_problem_greedy_algorithm(coin_denominations, money_amount):
     """
-
+    Function that solves the change making problem using a greedy algorithm
+    approach. The function  takes two arguments, a list or tuple of positive
+    integers, and a positive integer. It returns a list that contains
+    the coins that must be returned.
+    Each coint that must be returned is represented by its denomination.
+    Example:
+    change_making_problem_greedy_algorithm([1, 2, 7], 12)
+    will return a list [7, 2, 2, 1]
+    This means that you will return 4 coins with the respective
+    denominations.
     """
     check_input_data(coin_denominations, money_amount)
 
@@ -10,16 +19,13 @@ def change_making_problem_greedy_algorithm(coin_denominations, money_amount):
         money_left_for_change = money_amount
         current_coin_index = index
         feasible_solution = []
-        print(money_amount, index, feasible_solution)
         while True:
-            print(money_left_for_change)
 
             try:
                 current_coin_value = coin_denominations[current_coin_index]
             except:
-                raise EnvironmentError
+                raise NoFeasibleSolutionError
 
-            print(current_coin_value, current_coin_index, money_left_for_change)
             if money_left_for_change - current_coin_value > 0:
                 money_left_for_change -= current_coin_value
                 feasible_solution.append(current_coin_value)
@@ -31,6 +37,49 @@ def change_making_problem_greedy_algorithm(coin_denominations, money_amount):
                 current_coin_index += 1
 
 
-
 def check_input_data(coin_denominations, money_amount):
+    """
+    Funtion that checks the correctness of the given arguments.
+    The function arguments are considered correct if,
+    the coin_denominations argument is a list or a tuple
+    that cointains only positive integers.
+    money_amount is a positive integer.
+    """
+    if not isinstance(money_amount, int):
+        raise MoneyAmountNotAnIntegerError
+
+    if money_amount <= 0:
+        raise NonPositiveMoneyAmountError
+
+    if not isinstance(coin_denominations, (tuple, list)):
+        raise CoinDenominationsNotAListOrTupleError
+
+    for denomination in coin_denominations:
+        if not isinstance(denomination, int):
+            raise CoinDenominationNotAnIntegerError
+        elif denomination < 1:
+            raise CoinDenominationNonPositiveError
+
+
+class NoFeasibleSolutionError(Exception):
+    pass
+
+
+class MoneyAmountNotAnIntegerError(Exception):
+    pass
+
+
+class NonPositiveMoneyAmountError(Exception):
+    pass
+
+
+class CoinDenominationsNotAListOrTupleError(Exception):
+    pass
+
+
+class CoinDenominationNotAnIntegerError(Exception):
+    pass
+
+
+class CoinDenominationNonPositiveError(Exception):
     pass
--- algorithms/change_making_problem_greedy_algorithm_test.py
+++ algorithms/change_making_problem_greedy_algorithm_test.py
@@ -1,5 +1,5 @@
 import unittest
-import change_making_problem_greedy_algorithm
+import change_making_problem_greedy_algorithm as CMP_greedy
 
 
 class TestCoinsSolution(unittest.TestCase):
@@ -7,10 +7,69 @@ class TestCoinsSolution(unittest.TestCase):
     def test_coins_solution_exmp1(self):
         coin_denominations = [3, 8, 12]
         money_amount = 27
+        answer = CMP_greedy.change_making_problem_greedy_algorithm(
+            coin_denominations, money_amount)
+        self.assertEqual(answer, [12, 12, 3])
 
-        solution = change_making_problem_greedy_algorithm.change_making_problem_greedy_algorithm(coin_denominations, money_amount)
+    def test_coins_solution_exmp2(self):
+        coin_denominations = [1, 3, 5, 12, 48]
+        money_amount = 9
+        answer = CMP_greedy.change_making_problem_greedy_algorithm(
+            coin_denominations, money_amount)
+        self.assertEqual(answer, [5, 3, 1])
 
-        self.assertEqual(solution, [12, 12, 3])
+    def test_coins_solution_exmp3(self):
+        coin_denominations = [1, 3, 5, 12, 15]
+        money_amount = 55
+        answer = CMP_greedy.change_making_problem_greedy_algorithm(
+            coin_denominations, money_amount)
+        self.assertEqual(answer, [15, 15, 15, 5, 5])
+
+
+class TestCoinsExceptions(unittest.TestCase):
+
+    def test_no_feasible_solution_error(self):
+        coin_denominations = [2, 4, 8]
+        money_amount = 31
+        with self.assertRaises(CMP_greedy.NoFeasibleSolutionError):
+            CMP_greedy.change_making_problem_greedy_algorithm(
+                coin_denominations, money_amount)
+
+    def test_money_amount_not_integer(self):
+        coin_denominations = [1, 3, 5, 12, 48]
+        money_amount = "Money amount"
+        with self.assertRaises(CMP_greedy.MoneyAmountNotAnIntegerError):
+            CMP_greedy.change_making_problem_greedy_algorithm(
+                coin_denominations, money_amount)
+
+    def test_money_amount_non_positive(self):
+        coin_denominations = [1, 3, 5, 12, 48]
+        money_amount = 0
+        with self.assertRaises(CMP_greedy.NonPositiveMoneyAmountError):
+            CMP_greedy.change_making_problem_greedy_algorithm(
+                coin_denominations, money_amount)
+
+    def test_coin_denominations_not_list_tuple(self):
+        coin_denominations = {"one": 1}
+        money_amount = 15
+        with self.assertRaises(
+                CMP_greedy.CoinDenominationsNotAListOrTupleError):
+                    CMP_greedy.change_making_problem_greedy_algorithm(
+                        coin_denominations, money_amount)
+
+    def test_coin_denomination_not_integer(self):
+        coin_denominations = [1, 3, 5, "This is not an integer", 48]
+        money_amount = 15
+        with self.assertRaises(CMP_greedy.CoinDenominationNotAnIntegerError):
+            CMP_greedy.change_making_problem_greedy_algorithm(
+                coin_denominations, money_amount)
+
+    def test_coin_denomination_negative_int(self):
+        coin_denominations = [1, 3, -5, 16, 48]
+        money_amount = 15
+        with self.assertRaises(CMP_greedy.CoinDenominationNonPositiveError):
+            CMP_greedy.change_making_problem_greedy_algorithm(
+                coin_denominations, money_amount)
 
 if __name__ == '__main__':
     unittest.main()
