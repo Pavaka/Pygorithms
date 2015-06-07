e89d9fda313176f54b718bab5b72fd5a6d574b6b
Pavel <pavelsd@abv.bg>
2015-06-07 22:15:56 +0300

    Testing structure changed

---
+++ tests/knapsack_problem_DP_test.py
@@ -0,0 +1,37 @@
+import unittest
+import sys
+import os
+path = os.path.abspath("../algorithms")
+sys.path.append(path)
+from knapsack_problem_DP import knapsack_problem_DP
+
+
+class TestKnapsackDPoptimality(unittest.TestCase):
+
+    def test_knapsack_optimal_solution_exmp1(self):
+        items = [(3, 4), (2, 3), (4, 2), (4, 3)]
+        capacity = 6
+        answer = knapsack_problem_DP(items, capacity)
+        self.assertEqual(answer, 8)
+
+    def test_knapsack_optimal_solution_exmp2(self):
+        items = [(1, 1), (6, 2), (18, 5), (22, 6), (28, 7)]
+        capacity = 11
+        answer = knapsack_problem_DP(items, capacity)
+        self.assertEqual(answer, 40)
+
+    def test_knapsack_optimal_solution_exmp3(self):
+        items = [(1, 1), (6, 2), (18, 5), (22, 6), (28, 7)]
+        capacity = 0
+        answer = knapsack_problem_DP(items, capacity)
+        self.assertEqual(answer, 0)
+
+    def test_knapsack_optimal_solution_exmp4(self):
+        items = []
+        capacity = 15
+        answer = knapsack_problem_DP(items, capacity)
+        self.assertEqual(answer, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
