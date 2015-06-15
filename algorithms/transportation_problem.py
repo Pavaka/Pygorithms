8d1903b6e16513132a0fd1f4073aa7b49a0be834
Pavel <pavelsd@abv.bg>
2015-06-15 11:25:40 +0300

    Working first transportation table

--- algorithms/transportation_problem.py
+++ algorithms/transportation_problem.py
@@ -1,7 +1,7 @@
 balancing_flags = ("balanced", "additional row", "additional column")
 
 
-class transportationCell:
+class TransportationCell:
 
     def __init__(self, cost):
         self.cost = cost
@@ -13,7 +13,11 @@ def transportation_problem(costs=None, vector_a=None, vector_b=None):
     table_rows = len(vector_a)
     table_columns = len(vector_b)
     costs = convert_costs_to_two_dimensional(costs, table_rows, table_columns)
-    transportation_table, balancing_flag, vector_a, vector_b = create_transportation_table(costs, vector_a, vector_b)
+    empty_transportation_table, balancing_flag, vector_a, vector_b\
+        = create_transportation_table(costs, vector_a, vector_b)
+    transportation_table = find_first_transportation_table(
+        empty_transportation_table, vector_a, vector_b)
+
 
 
 def convert_costs_to_two_dimensional(costs, table_rows, table_columns):
@@ -48,16 +52,37 @@ def create_transportation_table(costs, vector_a, vector_b):
     for i in range(table_rows):
         row_values = []
         for j in range(table_columns):
-            row_values.append(transportationCell(costs[i][j]))
+            row_values.append(TransportationCell(costs[i][j]))
         transportation_table.append(row_values)
 
     if balancing_flag == balancing_flags[0]:
         return transportation_table, balancing_flag, vector_a, vector_b
     elif balancing_flag == balancing_flags[1]:
-        new_row = [transportationCell(0)] * table_columns
+        new_row = [TransportationCell(0)] * table_columns
         transportation_table.append(new_row)
     elif balancing_flag == balancing_flags[2]:
         for row in range(table_rows):
-            transportation_table[row].append(transportationCell(0))
+            transportation_table[row].append(TransportationCell(0))
 
     return transportation_table, balancing_flag, vector_a, vector_b
+
+
+def find_first_transportation_table(transportation_table, vector_a, vector_b):
+    i = 0
+    j = 0
+    last_transportation_table_cell = transportation_table[-1][-1]
+    while True:
+        Ai = vector_a[i]
+        Bi = vector_b[j]
+        theta = min(Ai, Bi)
+        transportation_table[i][j].amount = theta
+        vector_a[i] -= theta
+        vector_b[j] -= theta
+
+        if transportation_table[i][j] is last_transportation_table_cell:
+            return transportation_table
+
+        if vector_a[i] == 0:
+            i += 1
+        elif vector_b[j] == 0:
+            j += 1
--- tests/transportation_problem_test.py
+++ tests/transportation_problem_test.py
@@ -5,6 +5,7 @@ path = os.path.abspath("..")
 sys.path.append(path)
 from algorithms.transportation_problem import *
 
+
 class TestTransportationProblem(unittest.TestCase):
 
     def test_cots_to_two_dimensional(self):
@@ -14,7 +15,8 @@ class TestTransportationProblem(unittest.TestCase):
         answer_expected = [[1, 2, 3, 4],
                            [5, 6, 7, 8],
                            [9, 10, 11, 12]]
-        answer_returned = convert_costs_to_two_dimensional(costs, table_rows, table_columns)
+        answer_returned = convert_costs_to_two_dimensional(
+            costs, table_rows, table_columns)
         self.assertEqual(answer_returned, answer_expected)
 
 
@@ -27,65 +29,119 @@ class TestCreationTransportationTable(unittest.TestCase):
         self.vector_a = [100, 130, 170]
         self.vector_b = [150, 120, 80, 50]
 
-
-
     def test_create_transportation_table_balancig_flag(self):
 
-        balncing_flag = create_transportation_table(self.costs, self.vector_a, self.vector_b)[1]
+        balncing_flag = create_transportation_table(self.costs, self.vector_a,
+                                                    self.vector_b)[1]
         self.assertEqual(balncing_flag, balancing_flags[0])
         self.vector_a[1] += 10
-        balncing_flag = create_transportation_table(self.costs, self.vector_a, self.vector_b)[1]
+        balncing_flag = create_transportation_table(self.costs, self.vector_a,
+                                                    self.vector_b)[1]
         self.assertEqual(balncing_flag, balancing_flags[2])
         self.vector_b[2] += 30
-        balncing_flag = create_transportation_table(self.costs, self.vector_a, self.vector_b)[1]
+        balncing_flag = create_transportation_table(self.costs, self.vector_a,
+                                                    self.vector_b)[1]
         self.assertEqual(balncing_flag, balancing_flags[1])
 
     def test_create_transportation_table_vector_a(self):
         self.vector_a = [100, 130, 210]
-        result = create_transportation_table(self.costs, self.vector_a, self.vector_b)
-        result_vector_a, result_vector_b =result[2],result[3]
+        result = create_transportation_table(self.costs, self.vector_a,
+                                             self.vector_b)
+        result_vector_a, result_vector_b = result[2], result[3]
         self.assertEqual(result_vector_a, self.vector_a)
         self.assertEqual([150, 120, 80, 50, 40], result_vector_b)
 
     def test_create_transportation_table_vector_b(self):
         self.vector_b = [150, 120, 80, 100]
-        result = create_transportation_table(self.costs, self.vector_a, self.vector_b)
-        result_vector_a, result_vector_b =result[2],result[3]
+        result = create_transportation_table(self.costs, self.vector_a,
+                                             self.vector_b)
+        result_vector_a, result_vector_b = result[2], result[3]
         self.assertEqual(result_vector_b, self.vector_b)
         self.assertEqual([100, 130, 170, 50], result_vector_a)
 
     def test_transportation_table_balanced(self):
-        transportation_table = create_transportation_table(self.costs, self.vector_a, self.vector_b)[0]
+        transportation_table = create_transportation_table(
+            self.costs, self.vector_a, self.vector_b)[0]
         table_rows = len(transportation_table)
         table_columns = len(transportation_table[0])
         for i in range(table_rows):
             for j in range(table_columns):
-                self.assertEqual(transportation_table[i][j].cost, self.costs[i][j])
+                self.assertEqual(transportation_table[i][j].cost,
+                                 self.costs[i][j])
 
     def test_transportation_table_unbalanced_additional_row(self):
         self.vector_b = [150, 120, 80, 70]
-        transportation_table = create_transportation_table(self.costs, self.vector_a, self.vector_b)[0]
+        transportation_table = create_transportation_table(
+            self.costs, self.vector_a, self.vector_b)[0]
         table_rows = len(transportation_table)
         table_columns = len(transportation_table[0])
         for i in range(table_rows - 1):
             for j in range(table_columns):
-                self.assertEqual(transportation_table[i][j].cost, self.costs[i][j])
+                self.assertEqual(transportation_table[i][j].cost,
+                                 self.costs[i][j])
 
         for j in range(table_columns):
             self.assertEqual(transportation_table[-1][j].cost, 0)
 
     def test_transportation_table_unbalanced_additional_column(self):
         self.vector_a = [100, 130, 200]
-        transportation_table = create_transportation_table(self.costs, self.vector_a, self.vector_b)[0]
+        transportation_table = create_transportation_table(
+            self.costs, self.vector_a, self.vector_b)[0]
         table_rows = len(transportation_table)
         table_columns = len(transportation_table[0])
         last_column_index = table_columns - 1
         for i in range(table_rows):
             for j in range(table_columns):
                 if j != last_column_index:
-                    self.assertEqual(transportation_table[i][j].cost, self.costs[i][j])
+                    self.assertEqual(transportation_table[i][j].cost,
+                                     self.costs[i][j])
                 elif j == last_column_index:
                     self.assertEqual(transportation_table[i][j].cost, 0)
+
+
+class TestFirstTrnspTableFinder(unittest.TestCase):
+
+    def test_first_transportation_table_balanced(self):
+        costs = [[1, 2, 3, 4],
+                 [5, 6, 7, 8],
+                 [9, 10, 11, 12]]
+        vector_a = [100, 130, 170]
+        vector_b = [150, 120, 80, 50]
+        new_table = create_transportation_table(costs, vector_a, vector_b)[0]
+        first_transp_table = find_first_transportation_table(
+            new_table, vector_a, vector_b)
+
+        amounts = [[100, None, None, None],
+                   [50, 80, None, None],
+                   [None, 40, 80, 50]]
+
+        for i in range(len(vector_a)):
+            for j in range(len(vector_b)):
+                self.assertEqual(
+                    first_transp_table[i][j].amount, amounts[i][j])
+                self.assertEqual(first_transp_table[i][j].cost, costs[i][j])
+
+    def test_first_transportation_table_unbalanced(self):
+        costs = [[1, 2, 3, 4],
+                 [5, 6, 7, 8],
+                 [9, 10, 11, 12]]
+        vector_a = [100, 130, 200]
+        vector_b = [150, 120, 80, 50]
+
+        amounts = [[100, None, None, None, None],
+                   [50, 80, None, None, None],
+                   [None, 40, 80, 50, 30]]
+
+        new_table = create_transportation_table(costs, vector_a, vector_b)[0]
+        first_transp_table = find_first_transportation_table(
+            new_table, vector_a, vector_b)
+
+        for i in range(len(vector_a)):
+            for j in range(len(vector_b)):
+                self.assertEqual(
+                    first_transp_table[i][j].amount, amounts[i][j])
+
+
 
 if __name__ == '__main__':
     unittest.main()
